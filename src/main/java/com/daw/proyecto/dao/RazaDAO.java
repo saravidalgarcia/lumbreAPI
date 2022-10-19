6eaaef2c177f1392557ed850da580daca2bcc1eb
Sara Vidal García <72193038+saravidalgarcia@users.noreply.github.com>
2022-10-19 06:56:43 +0200

    Añadido DAO para las razas

---
+++ src/main/java/com/daw/proyecto/dao/RazaDAO.java
@@ -0,0 +1,20 @@
+package com.daw.proyecto.dao;
+
+import java.util.List;
+
+import com.daw.proyecto.entity.Raza;
+import com.daw.proyecto.entity.Usuario;
+
+public interface RazaDAO {
+	
+	public List<Raza> getRazas(Usuario usuario);
+
+	public String crearRaza(Raza raza);
+	
+	public Raza getRaza(Long id);
+	
+	public String updateRaza(Raza raza);
+	
+	public String deleteRaza(Long id);
+	
+}
