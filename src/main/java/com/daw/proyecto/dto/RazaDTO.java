40202e7d569ed9d307ebe3edf6f1bd9e54cf9e64
Sara Vidal García <72193038+saravidalgarcia@users.noreply.github.com>
2022-10-19 08:27:10 +0200

    Añadido DTO para raza

---
+++ src/main/java/com/daw/proyecto/dto/RazaDTO.java
@@ -0,0 +1,91 @@
+package com.daw.proyecto.dto;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class RazaDTO implements Serializable{
+
+	//Atributos
+	
+	private Long id;
+	private String denominacion;
+	private String tipo;
+	private String descripcion;
+	private String creacion;
+	private String modificacion;
+	
+	
+	//Constructores
+	
+	public RazaDTO() {}
+
+	public RazaDTO(Long id, String denominacion, String tipo, String descripcion, String creacion, String modificacion) {
+		this.id = id;
+		this.denominacion = denominacion;
+		this.tipo = tipo;
+		this.descripcion = descripcion;
+		this.creacion = creacion;
+		this.modificacion = modificacion;
+	}
+
+	
+	//Getters y setters
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDenominacion() {
+		return denominacion;
+	}
+
+	public void setDenominacion(String denominacion) {
+		this.denominacion = denominacion;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	public String getCreacion() {
+		return creacion;
+	}
+
+	public void setCreacion(String creacion) {
+		this.creacion = creacion;
+	}
+
+	public String getModificacion() {
+		return modificacion;
+	}
+
+	public void setModificacion(String modificacion) {
+		this.modificacion = modificacion;
+	}
+
+	
+	//toString
+	
+	@Override
+	public String toString() {
+		return "RazaDTO [id=" + id + ", denominacion=" + denominacion + ", tipo=" + tipo + ", descripcion="
+				+ descripcion + ", creacion=" + creacion + ", modificacion=" + modificacion + "]";
+	}
+
+}
--- src/main/java/com/daw/proyecto/utils/EntityToDTO.java
+++ src/main/java/com/daw/proyecto/utils/EntityToDTO.java
@@ -1,6 +1,8 @@
 package com.daw.proyecto.utils;
 
+import com.daw.proyecto.dto.RazaDTO;
 import com.daw.proyecto.dto.UsuarioDTO;
+import com.daw.proyecto.entity.Raza;
 import com.daw.proyecto.entity.Usuario;
 
 public class EntityToDTO {
@@ -15,6 +17,22 @@ public class EntityToDTO {
 		dto.setId(e.getId());
 		dto.setEmail(e.getEmail());
 		dto.setUsername(e.getUsername());
+		return dto;
+	}
+	
+	/**
+	 * Recibe una raza (entidad) y devuelve un objeto de la clase RazaDTO
+	 * @param e
+	 * @return
+	 */
+	public static RazaDTO toRazaDTO(Raza e) {
+		RazaDTO dto = new RazaDTO();
+		dto.setCreacion(e.getCreacion() != null ? e.getCreacion().toString(): null);
+		dto.setDenominacion(e.getDenominacion());
+		dto.setDescripcion(e.getDescripcion());
+		dto.setId(e.getId());
+		dto.setModificacion(e.getModificacion() != null ? e.getModificacion().toString(): null);
+		dto.setTipo(e.getTipo());
 		return dto;
 	}
 	
