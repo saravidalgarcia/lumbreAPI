8840a0fbf649f2fb81928757fe304d35371f499f
Sara Vidal García <72193038+saravidalgarcia@users.noreply.github.com>
2022-10-20 08:17:55 +0200

    Añadido DAO para sesión

---
+++ src/main/java/com/daw/proyecto/dao/SesionDAO.java
@@ -0,0 +1,23 @@
+package com.daw.proyecto.dao;
+
+import java.util.List;
+
+import com.daw.proyecto.entity.Sesion;
+import com.daw.proyecto.entity.Usuario;
+import com.daw.proyecto.entity.Campanha;
+
+public interface SesionDAO {
+
+	public String crearSesion(Sesion sesion);
+	
+	public Sesion getSesion(Long id, Campanha campanha);
+	
+	public String updateSesion(Sesion sesion);
+	
+	public String deleteSesion(Long id, Campanha campanha);
+	
+	public List<Sesion> getSesiones(Usuario usuario);
+	
+	public List<Sesion> getSesiones(Campanha campanha);
+	
+}
